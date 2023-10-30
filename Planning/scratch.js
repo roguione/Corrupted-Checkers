2b5d96b649506a44391e830f3b03a69204f3b6ee
roguione <myjobcontacts@yahoo.com>
2023-10-30 17:10:13 -0500

    maybe?

--- Planning/scratch.js
+++ Planning/scratch.js
@@ -13,6 +13,56 @@ const board = [
   [1, 0, 1, 0, 1, 0, 1, 0]
 ];
 
+function makeBoard() {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      let newCell = document.createElement('div');
+      // additional attributes
+      newCell.setAttribute('data-x', j);
+      newCell.setAttribute('data-y', i);
+      newCell.classList.add('cell');
+      board.append(newCell);
+    }
+  }
+}
+
+// global vars
+let grid;
+
+// cached dom elements
+const board = document.querySelector('#grid-container');
+
+// utility / helper function
+function makeBoard() {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      let newCell = document.createElement('div');
+      // additional attributes
+      newCell.setAttribute('data-x', j);
+      newCell.setAttribute('data-y', i);
+      newCell.classList.add('cell');
+      board.append(newCell);
+    }
+  }
+}
+
+init();
+
+function init() {
+  grid = new Array(8).fill().map(() => new Array(8).fill("0"));
+
+  makeBoard();
+
+  // clean up
+  board.removeEventListener('click', checkCell);
+  // add again
+  board.addEventListener('click', checkCell);
+}
+
+function checkCell(e) {
+  console.log(e.target.dataset);
+}
+
 // test board 4x4
 const brd = [
   [0, -1, 0, -1],
@@ -21,3 +71,31 @@ const brd = [
   [-1, 0, -1, 0]
 ];
 
+function makeBoard() {
+  const board = document.getElementById('board'); // Assuming you have an element with the id 'board' to append cells to.
+
+  const brd = [
+    [0, -1, 0, -1],
+    [-1, 0, -1, 0],
+    [0, -1, 0, -1],
+    [-1, 0, -1, 0]
+  ];
+
+  for (let i = 0; i < brd.length; i++) {
+    for (let j = 0; j < brd[i].length; j++) {
+      let newCell = document.createElement('div');
+      newCell.setAttribute('data-x', j);
+      newCell.setAttribute('data-y', i);
+      newCell.classList.add('cell');
+
+      if (brd[i][j] === -1) {
+        newCell.classList.add('obstacle'); // You can add an 'obstacle' class for -1 values.
+      }
+
+      board.append(newCell);
+    }
+  }
+}
+
+// Call the function to create the board
+makeBoard();
--- js/game.js
+++ js/game.js
@@ -1,3 +1,41 @@
 console.log('javascript is a go');
 
+// global vars
+let grid;
+
+// DOM elements
+const board = document.querySelector('#grid -container');
+
+// util/ help function
+function makeBoard(){
+    for(let i = 0; i < grid.length; i++){
+        for(let j = 0; j <grid[i].length; j++){
+            let newCell = document.createElement('div');
+        // additional elements
+        newCell.setAttribute('data-x', j);
+        newCell.setAttribute('data-y', i);
+        newCell.classList.add('cell');
+        board.appendChild(newCell);
+        }
+    }
+}
+
+init();
+
+function init(){
+    grid = new Array(8).fill().map(() => new Array(8).fill('0');
+
+    makeBoard();
+
+    // cleanup
+    board.removeEventListener('click', checkCell);
+    // addnew
+    board.addEventListener('click', checkCell);
+}
+
+function checkCell(e) {
+    console.log(e.target.dataset);
+}
+
+
 
