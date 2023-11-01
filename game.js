2f5e7feb3065255c06bfd765e60ee4958b387d1a
roguione <myjobcontacts@yahoo.com>
2023-11-01 08:11:16 -0500

    game script

--- game.js
+++ game.js
@@ -1,18 +1,14 @@
-// Logging to console for debugging purposes
 console.log("javascript is up");
 
 // 1. Constants
-
-// Get DOM elements for the game board, start button, message display, and new game button
 const gridContainer = document.querySelector('#grid-container');
 const startButton = document.querySelector('#start-button');
 const messageDisplay = document.querySelector('#message');
 const newGameButton = document.querySelector('#new-game-button');
+const scoreBoxPlayer1 = document.querySelector('#score-player1');
+const scoreBoxComputer = document.querySelector('#score-computer');
 
 // 2. Variables
-
-// Set up primary game state variables, such as the game grid, 
-// the current selected checker piece, current player's turn, players' scores, and game status
 let grid = [];
 let selectedChecker = null;
 let playerTurn = 1;
@@ -21,19 +17,20 @@ let gameStarted = false;
 
 // 3. Board Functions
 
-// Initializes the 8x8 game grid. It assigns player pieces and empty spots
+// Initializes the 8x8 game grid with players' pieces and empty spots
 function initializeBoard() {
+    console.log('Initializing board');
     for (let i = 0; i < 8; i++) {
         grid[i] = [];
         for (let j = 0; j < 8; j++) {
             // Assign checkers for OG
             if ((i + j) % 2 !== 0 && i < 3) {
                 grid[i][j] = 2;
-            } 
+            }
             // Assign checkers for Player 1
             else if ((i + j) % 2 !== 0 && i > 4) {
                 grid[i][j] = 1;
-            } 
+            }
             // Empty spots
             else {
                 grid[i][j] = 0;
@@ -42,8 +39,9 @@ function initializeBoard() {
     }
 }
 
-// Constructs the visual representation of the game board on the page
+// Renders the game board visually on the page
 function createBoard() {
+    console.log('Creating board');
     for (let row = 0; row < 8; row++) {
         for (let col = 0; col < 8; col++) {
             const cell = createCell(row, col);
@@ -58,101 +56,85 @@ function createBoard() {
     }
 }
 
-// Create an individual cell (square) for the game board
+// Generates a cell (square) for the board with its attributes and event listener
 function createCell(row, col) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     cell.setAttribute('data-x', col);
     cell.setAttribute('data-y', row);
 
-    // Apply different styles for the playable squares
+    // Style playable squares
     if ((row + col) % 2 === 1) {
         cell.classList.add('light-green');
     }
 
-    // Add event listener to handle cell clicks
+    // Event listener for cell clicks
     cell.addEventListener('click', cellClick);
 
     return cell;
 }
 
-// Generate a checker piece for a specified player
+// Creates a checker piece with its attributes and event listener
 function createChecker(row, col, player) {
     const checker = document.createElement('div');
     checker.classList.add('checker', `player${player}`);
     checker.setAttribute('data-player', player);
 
-    // Add event listener to handle checker piece clicks
+    // Event listener for checker clicks
     checker.addEventListener('click', checkerClick);
 
     return checker;
 }
 
-// Logic for when a checker piece is clicked on
+// Handles the logic when a checker piece is clicked
 function checkerClick(e) {
-    // If the game hasn't started yet, don't proceed
+    console.log('Checker clicked');
     if (!gameStarted) return;
 
-    console.log("Checker clicked");
-
-    // Prevent event bubbling to parent elements
+    // Prevent event from propagating upwards
     e.stopPropagation();
 
-    // If there's a previously selected checker, deselect it
+    // Deselect any previously selected checker
     if (selectedChecker) {
         selectedChecker.classList.remove('selected');
     }
 
-    // Set the current checker as the selected checker
+    // Mark this checker as the selected one
     selectedChecker = e.target;
     selectedChecker.classList.add('selected');
 }
 
-// Defines the logic for when a cell (square) on the board is clicked
+// Handles the logic when a cell on the board is clicked
 function cellClick(e) {
-    if (!gameStarted) return;
+    console.log('Cell clicked');
+    if (!gameStarted || !selectedChecker) return;
 
-    console.log("Cell clicked");
-
-    // Get positions of the selected checker and the clicked cell
     const [fromRow, fromCol] = getCheckerPosition(selectedChecker);
     const [toRow, toCol] = getCellPosition(e.currentTarget);
 
-    // Check if the move is valid.
     if (isValidMove(fromRow, fromCol, toRow, toCol)) {
-        console.log("Valid move made");
         moveChecker(fromRow, fromCol, toRow, toCol);
-
-        // If it's a jump move, increase the score
-        if (Math.abs(toRow - fromRow) === 2 && Math.abs(toCol - fromCol) === 2) {
-            scores[playerTurn]++;
-        }
-
-        // Check for game-ending conditions
-        if (toRow === 0 || toRow === 7) {
-            declareWinner(playerTurn);
+        if (playerTurn === 1 && toRow === 0) {
+            declareWinner(playerTurn, true);
             return;
         }
-
-        // Switch to OG
-
-        // If it's OOOG's turn, let the OG make a move
+        if (playerTurn === 2 && toRow === 7) {
+            declareWinner(playerTurn, true);
+            return;
+        }
+        switchPlayer();
         if (playerTurn === 2) {
             computerPlay();
         }
-
-        // Refresh the board to reflect the current game state
-        updateBoard();
-    }    
+    }
 }
 
-// A basic AI function for the computer to randomly select a valid move.....intnet find
+// Basic AI: The computer randomly selects a valid move
 function computerPlay() {
-    console.log("OG's turn");
-
+    console.log('OG`s Turn');
     let validMoves = [];
 
-    // Loop through the grid to find valid moves for OOOG
+    // Identify all valid moves for OG
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
             if (grid[i][j] === 2) {
@@ -162,55 +144,85 @@ function computerPlay() {
         }
     }
 
-    // If there are valid moves available, select OG one at random
+    // Make a random move if there are valid ones available
     if (validMoves.length > 0) {
         const randomMove = validMoves[Math.floor(Math.random() * validMoves.length)];
         moveChecker(randomMove[0], randomMove[1], randomMove[2], randomMove[3]);
 
-        // Check for game-ending conditions for OG
+        // If the move results in a win for OG, declare it
         if (randomMove[2] === 0 || randomMove[2] === 7) {
             declareWinner(playerTurn);
         }
 
-        // Switch to the other player and update the board
+        // Switch turns
         switchPlayer();
         updateBoard();
     }
 }
 
-// Determine the valid moves a checker piece can make from a given position
+// Identify valid moves a checker can make from its current position
 function getValidMoves(row, col) {
     const direction = playerTurn === 1 ? -1 : 1;
     let validMoves = [];
 
-    // Check for potential jump moves
+    // Identify potential jump moves
     if (isValidMove(row, col, row + 2 * direction, col + 2)) {
         validMoves.push([row, col, row + 2 * direction, col + 2]);
     }
     if (isValidMove(row, col, row + 2 * direction, col - 2)) {
         validMoves.push([row, col, row + 2 * direction, col - 2]);
     }
+    
+    // Add simple moves for the computer
+    if (playerTurn === 2) {
+        if (isValidMove(row, col, row + direction, col + 1)) {
+            validMoves.push([row, col, row + direction, col + 1]);
+        }
+        if (isValidMove(row, col, row + direction, col - 1)) {
+            validMoves.push([row, col, row + direction, col - 1]);
+        }
+    }
 
     return validMoves;
 }
 
-// Log the determined valid moves for debugging purposes
-console.log('validMoves');
+// Validates whether a move is valid or not
+function isValidMove(fromRow, fromCol, toRow, toCol) {
+    console.log(`Checking validity of move from (${fromRow}, ${fromCol}) to (${toRow}, ${toCol})`);
 
-// Execute the movement of a checker piece on the game board
+    const direction = playerTurn === 1 ? -1 : 1;
+    const oppositePlayer = playerTurn === 1 ? 2 : 1;
+
+    if (Math.abs(toRow - fromRow) !== Math.abs(toCol - fromCol)) {
+        return false;
+    }
+
+    if (toRow - fromRow === direction && Math.abs(toCol - fromCol) === 1) {
+        return grid[toRow][toCol] === 0;
+    }
+
+    if (toRow - fromRow === 2 * direction && Math.abs(toCol - fromCol) === 2) {
+        const midRow = (fromRow + toRow) / 2;
+        const midCol = (fromCol + toCol) / 2;
+        return grid[midRow][midCol] === oppositePlayer && grid[toRow][toCol] === 0;
+    }
+    
+    return false;
+}
+
+// click Moves a checker on the board
 function moveChecker(fromRow, fromCol, toRow, toCol) {
-    console.log(`Move checker from (${fromRow}, ${fromCol}) to (${toRow}, ${toCol})`);
-
-    // Move the checker in the game grid.
+    console.log(`Mv chkr (${fromRow}, ${fromCol}) to (${toRow}, ${toCol})`);
+    // Update the logical grid
     grid[toRow][toCol] = grid[fromRow][fromCol];
     grid[fromRow][fromCol] = 0;
 
-    // If it's a jump move, remove the jumped-over piece
+    // If it's a jump, remove the checker that was jumped over
     if (Math.abs(toRow - fromRow) === 2 && Math.abs(toCol - fromCol) === 2) {
         grid[(fromRow + toRow) / 2][(fromCol + toCol) / 2] = 0;
     }
 
-    // Remove selection styling from the moved checker
+    // Clear selection styling
     if (selectedChecker) {
         selectedChecker.classList.remove('selected');
     }
@@ -218,18 +230,20 @@ function moveChecker(fromRow, fromCol, toRow, toCol) {
     selectedChecker = null;
 }
 
-// Declare the winner of the game
-function declareWinner(player) {
-    messageDisplay.textContent = `Player ${player} wins!`;
+// Proclaims a player as the winner
+function declareWinner(player, isKing = false) {
+    if (isKing) {
+        messageDisplay.textContent = `C-BBG King! Player ${player} wins!`;
+    }
     gameStarted = false;
 }
 
-// Switch the turn to the other player
+// Switches the turn between players
 function switchPlayer() {
     playerTurn = playerTurn === 1 ? 2 : 1;
 }
 
-// Reset the game state to its initial configuration,..intnet find that i tweaked to fit my game
+// Resets the game state
 function resetGame() {
     grid = [];
     selectedChecker = null;
@@ -238,82 +252,42 @@ function resetGame() {
     gameStarted = false;
     messageDisplay.textContent = '';
     gridContainer.innerHTML = '';
-
+    // scoreBoxPlayer1.textContent = '';
+    // scoreBoxComputer.textContent = '';
     initializeBoard();
     createBoard();
 }
 
-// Refresh the visual representation of the game board
+// Updates the scores for both players
+function updateScores() {
+    scoreBoxPlayer1.textContent = scores[1];
+    scoreBoxComputer.textContent = scores[2];
+}
+
+// Extracts checker's position from its attributes
+function getCheckerPosition(checker) {
+    const parent = checker.parentElement;
+    return getCellPosition(parent);
+}
+
+// Extracts cell's position from its attributes
+function getCellPosition(cell) {
+    return [parseInt(cell.getAttribute('data-y')), parseInt(cell.getAttribute('data-x'))];
+}
+
+// Updates the visual board after a move
 function updateBoard() {
     gridContainer.innerHTML = '';
     createBoard();
 }
 
-// 4. Utility Functions
-
-// Check if the move from a starting position to an ending position is valid
-function isValidMove(fromRow, fromCol, toRow, toCol) {
-    const direction = playerTurn === 1 ? -1 : 1;
-
-    // If the move is outside the game board or onto an occupied square, it's invalid
-    if (toRow < 0 || toRow > 7 || toCol < 0 || toCol > 7 || grid[toRow][toCol] !== 0) {
-        return false;
-    }
-
-    // Check for a simple move one square forward
-    if (toRow === fromRow + direction && (toCol === fromCol + 1 || toCol === fromCol - 1)) {
-        return true;
-    }
-
-    // Check for a jump move two squares forward
-    if (toRow === fromRow + 2 * direction) {
-        if (toCol === fromCol + 2 && grid[fromRow + direction][fromCol + 1] !== 0 && grid[fromRow + direction][fromCol + 1] !== playerTurn) {
-            return true;
-        }
-        if (toCol === fromCol - 2 && grid[fromRow + direction][fromCol - 1] !== 0 && grid[fromRow + direction][fromCol - 1] !== playerTurn) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-// Get the position of a checker piece
-function getCheckerPosition(checker) {
-    const cell = checker.parentNode;
-    return getCellPosition(cell);
-}
-
-// Get the position of a cell (square) on the board
-function getCellPosition(cell) {
-    return [parseInt(cell.getAttribute('data-y')), parseInt(cell.getAttribute('data-x'))];
-}
-
-// 5. Event Listeners
-
-// Handle the clicking of the start button to begin the game
-startButton.addEventListener('click', () => {
-    console.log("Commence");
-    gameStarted = true;
-    messageDisplay.textContent = 'Player 1\'s turn';
-});
-
-
-// Handle the clicking of the new game button to reset the game state
-newGameButton.addEventListener('click', () => {
-    console.log("New Duel");
+// 4. Event Listeners
+startButton.addEventListener('click', function () {
     resetGame();
+    gameStarted = true;
 });
+newGameButton.addEventListener('click', resetGame);
 
-/* // you have other game logic, you can continue with that
-newGameButton.addEventListener('click', () => {
-    // Reset game logic here
-    messageDisplay.textContent = 'New game! Player 1\'s turn';
-    // And any other logic to reset the game board
-}); */
-
-// 6. Initialization
-
-// Initialize the game board when the page loads
+// 5. Initial Setup
 initializeBoard();
 createBoard();
